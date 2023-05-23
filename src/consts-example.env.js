90a6fd5873b280ee6ae54d2f23593352c7539169
Pedro Trilles Machado <trillessky@gmail.com>
2023-05-23 11:48:12 -0300

    added example.env

---
+++ src/consts-example.env.js
@@ -0,0 +1,14 @@
+exports.NODE_ENV='development'
+
+exports.DB_NAME='schoolDb'
+exports.DB_HOST='localhost'
+exports.DB_PORT=3306
+exports.DB_USER='root'
+exports.DB_PASSWORD='suaSenha'
+exports.MYSQLDIALECT='mysql'
+exports.JWT_SECRET='seuSegredo'
+
+exports.SQLITEDIALECT='sqlite'
+exports.STORAGE='db.sqlite'
+
+// renomeie este arquivo para "consts.env.js" e altere os valores para os seus pessoais
